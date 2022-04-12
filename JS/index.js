8cc944de2905c6f615bcf1e8346c13fe7fdcd21e
Marcelo Alos <marceloalos@gmail.com>
2022-04-12 10:37:15 -0300

    Create repository

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+function Converter() {
+    var distanciaKm = document.getElementById("distanciaKm");
+    var distancia = distanciaKm.value;
+    var distanciaKmNumerico = parseInt(distancia);
+  
+    var distanciaEmAnosLuz = distanciaKmNumerico / 9460528405000;
+  
+    var nome = document.getElementById("nomeDoAstro");
+    var nomeAstro = nome.value;
+  
+    var elementoConvertido = document.getElementById("distanciaConvertida");
+    var AnosLuz =
+      "A distãncia entre " +
+      nomeAstro +
+      " e a Terra é de " +
+      distanciaEmAnosLuz.toFixed() +
+      " anos-luz";
+    elementoConvertido.innerHTML = AnosLuz;
+  }
+  
